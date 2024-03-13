038fce2adcb7a773545e0794fd7912062e02ee05
darayaq-01 <daniel@darayaq.de>
2024-03-13 12:46:08 +0100

    Add links to social media and update menu items

---
+++ src/consts/menuItems.d.ts
@@ -0,0 +1,37 @@
+export const menuItems = [
+    {
+        title: 'Home',
+        path: '/',
+        active: true,
+    },
+    {
+        title: 'Vita',
+        path: '/vita',
+        active: false,
+    },
+    {
+        title: 'Werke',
+        path: '/werke',
+        active: false,
+    },
+    {
+        title: 'Aktuelles',
+        path: '/aktuelles',
+        active: false,
+    },
+    {
+        title: 'Archiv',
+        path: '/archiv',
+        active: false,
+    },
+    {
+        title: 'Presse',
+        path: '/presse',
+        active: false,
+    },
+    {
+        title: 'Kontakt',
+        path: '/kontakt',
+        active: true,
+    },
+];
